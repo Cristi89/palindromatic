b5aeac5620ce9ff70b7e1025a1a1c12d76dfe447
Cristi Scheye <cristi.scheye@gmail.com>
2013-08-24 16:05:10 -0700

    Added user input form

--- app/controllers/palindromes_controller.rb
+++ app/controllers/palindromes_controller.rb
@@ -2,4 +2,18 @@ class PalindromesController < ApplicationController
 	def index
 		@palindromes = Palindrome.all
 	end
+
+	def new
+		@palindrome = Palindrome.new
+	end
+
+	def create
+		Palindrome.create(palindromes_params)
+		redirect_to palindromes_path
+	end
+
+	def palindromes_params
+		params.require(:palindrome).permit(:word)
+	end
+
 end
